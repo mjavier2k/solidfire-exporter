16565345be4c0e3d19f46251851909cb63ae3763
Marvin Javier <mjavier2k@gmail.com>
2020-04-13 12:04:08 -0500

    fix api call for cluster faults

--- pkg/solidfire/solidfire.go
+++ pkg/solidfire/solidfire.go
@@ -130,7 +130,7 @@ func (s *Client) GetClusterCapacity() (GetClusterCapacityResponse, error) {
 
 func (s *Client) ListClusterActiveFaults() (ListClusterFaultsResponse, error) {
 	payload := &RPCBody{
-		Method: "ListClusterActiveFaults",
+		Method: "ListClusterFaults",
 		Params: ListClusterFaultsRPCParams{
 			FaultTypes:    "current",
 			BestPractices: true,
@@ -145,6 +145,8 @@ func (s *Client) ListClusterActiveFaults() (ListClusterFaultsResponse, error) {
 		return r, err
 	}
 	err = json.Unmarshal(bodyBytes, &r)
+
+	log.Infoln(r)
 
 	if err != nil {
 		return r, err
